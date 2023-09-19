5b2290739e04c76cf40c93b20ca6794dfa7286d3
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-09-19 12:43:16 -0400

    adding to app.py and models.py

--- server/app.py
+++ server/app.py
@@ -8,19 +8,133 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    def post(self):
+        username = request.get_json().get('username')
+        password = request.get_json().get('password')
+        image_url = request.get_json().get('image_url')
+        bio = request.get_json().get('bio')
+        if not username or not password:
+            return {'error': 'invalid username'}, 422
+        
+        new_user = User(
+            username=username,
+            image_url=image_url,
+            bio=bio
+        )
+        new_user.password_hash = password
 
+        db.session.add(new_user)
+        db.session.commit()
+
+        session['user_id'] = new_user.id
+
+        response_json = {
+            'id': new_user.id,
+            'username': new_user.username,
+            'bio':new_user.bio,
+            'image_url':new_user.image_url
+        }
+        return response_json, 201
+
+    
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            response_json = {
+                'id': user.id,
+                'username': user.username,
+            }
+            return response_json, 200
+        
+        return {}, 401
+
 
 class Login(Resource):
-    pass
+    def post(self):
+        
+        username = request.get_json()['username']
+        user = User.query.filter(User.username == username).first()
+        
+        if user:
+            password = request.get_json()['password']
+
+
+            # users pwd is set by calling user.pawd = "new_pwd"
+            # instead of pwd = user.pwd , here we authenticate by using bcrypt checking pwd = stored pwd hash
+            if user.authenticate(password):
+            
+                session['user_id'] = user.id
+                return {
+                    'id':user.id,
+                    'username':user.username,
+                    'image_url':user.image_url,
+                    'bio':user.bio
+                }, 201
+
+            return {}, 200
+        return {},401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        if session['user_id']:
+            session['user_id'] = None
+            
+            return {}, 204
+        else:
+            return {}, 401
 
 class RecipeIndex(Resource):
-    pass
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            recipe_list = []
+            for recipe in Recipe.query.all():
+                recipe_dict = {
+                    'title': recipe.title,
+                    'instructions':recipe.instructions,
+                    'minutes_to_complete':recipe.minutes_to_complete,
+                    'user_id': session['user_id']
+                }
+                recipe_list.append(recipe_dict)
+            return recipe_list,200
+        return {},401
+
+    def post(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            data = request.get_json()
+            # title=data['title']
+            # instructions=data['instructions']
+            # minutes_to_complete=data['minutes_to_complete']
+            # user_id = session['user_id']
+            # recipes = Recipe.query.filter(Recipe.user_id == user_id).all()
+            # for recipe in recipes:
+            #     user = User.query.filter(User.id == user_id).first()
+            try:
+                new_recipe = Recipe(
+                    title=data['title'],
+                    instructions=data['instructions'],
+                    minutes_to_complete=data['minutes_to_complete'],
+                    user_id = session['user_id']
+                )
+
+                db.session.add(new_recipe)
+                db.session.commit()
+            except IntegrityError:
+                return {},422
+
+            return(
+                {
+                    'title': new_recipe.title,
+                    'instructions':new_recipe.instructions,
+                    'minutes_to_complete':new_recipe.minutes_to_complete,
+                    'user_id':new_recipe.user_id
+                }
+            ),201
+        return {}, 401
+    
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
--- server/models.py
+++ server/models.py
@@ -1,14 +1,75 @@
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
-
+from sqlalchemy.orm import validates
 from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    recipes = db.relationship('Recipe', backref='user')
+
+    def __repr__(self):
+        return f'<{self.id}. User {self.username}>'
+    
+    # validate the user's username to ensure that it is present and unique (no two users can have the same username).
+    # decorator validates is used , mean this method will be called whenever 'username' attribute is set
+    @validates('username')
+    def validate_username(self, key, username):
+        # validation logic, ensure username is present, unique.
+
+        #checking username is present
+        if not username:
+            raise ValueError("Username must be present")
+        #checking username is unique
+        user_exist = User.query.filter(User.username == username).first()
+        if user_exist and user_exist.id != self.id:
+            raise ValueError("Username is not unique")
+        # if user is unique and present return username
+        return username
+
+    # this is a special property decorator for sqlalchemy
+    # it leaves all of the sqlalchemy characteristics of the column in place
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("password may not be viewed")
+
+    # setter method for the password property
+    @password_hash.setter
+    def password_hash(self, password):
+        # utf-8 encoding and decoding is required in python 3
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    # authentication method using user and password
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
+    __table_args__ = (
+        db.CheckConstraint('length(instructions) >= 50'),
+    )
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String, nullable=False)
+    minutes_to_complete = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     
-    pass
+    def __repr__(self):
+        return f'<{self.id}. Recipe {self.title} has {self.instructions} belongs to {self.user_id}>'
+
+    #decorator validates is used , mean this method will be called whenever 'title', attribute is set
+    @validates('title')
+    def validate_title(self, key, title):
+        if not title:
+            raise ValueError("Title must be present")
+        return title
